ceaccaa43b08fd08ef4d5f2914cb44dda6dd0541
manduks <iam@armando.mx>
2019-05-29 21:51:26 -0500

    added naviation function

--- main/main.ino
+++ main/main.ino
@@ -1,37 +1,32 @@
 #include "Ultrasonic.h"
 #include "SimpleTimer.h"
 #include "Motors.h"
-#include "DigitalIO.h"
 
 SimpleTimer timer;
 
-// Ultrasonic usonic(13, 12);
+Ultrasonic usonic(13, 12);
 
-//  void sensorRead() {
-//    float distance = usonic.read();
-//    Serial.println("Ultrasonic: " + String(distance, DEC) + "[cm]");
-//  }
 
-DigitalIO lineSensor1(A5, INPUT);
-DigitalIO lineSensor2(A4, INPUT);
-DigitalIO lineSensor3(A3, INPUT);
+void navigate() {
+  float distance = usonic.read();
+  long randNumber = random(0, 2);
 
-void ioTest() {
- uint8_t state1 = lineSensor1.read();
- uint8_t state2 = lineSensor2.read();
- uint8_t state3 = lineSensor3.read();
- Serial.println("Sensor Status: " + String(state1, DEC) + ", " + String(state2, DEC) + ", " + String(state3, DEC));
+  if(distance < 20.00) {
+    if(randNumber > 0) {
+      turnRight();
+    } else {
+      turnLeft();
+    }
+  } else {
+    moveForward(64);
+  }
 }
 
 void setup() {
-  // initializeMotors();
-  Serial.begin(115200);
-  Serial.println("---> Ultrasonic test");
-  // timer.setInterval(500, &sensorRead);
-  timer.setInterval(500, &ioTest);
+  initializeMotors();
+  timer.setInterval(500, &navigate);
 }
 
 void loop() {
   timer.run();
-  // motorsTest();
 }
